508ae15e4574849c5f0fe7551872cd80b94f0a74
kchen36 <kchen36@stuy.edu>
2016-11-30 18:07:36 -0500

    version2

--- 01Library/Book.java
+++ 01Library/Book.java
@@ -5,53 +5,80 @@ public class Book{
     public Book(){
     }
     public Book(String a,String t, String i){
-	author = a;
-	title = t;
-	ISBN = i;
+    author = a;
+    title = t;
+    ISBN = i;
     }
     public String getAuthor(){
-	return author;
+    return author;
     }
     public String getTitle(){
-	return title;
+    return title;
     }
     public String getISBN(){
-	return ISBN;
+    return ISBN;
     }
     public void setAuthor(String s){
-	author = s;
+    author = s;
     }
     public void setTitle(String s){
-	title = s;
+    title = s;
     }
     public void setISBN(String s){
-	ISBN = s;
+    ISBN = s;
     }
     public String toString(){
-	return title + ", "+author +", "+ISBN
+    return title + ", "+author +", "+ISBN;
     }
 }
-abstract Class LibraryBook extends Book implements Comparable<LibraryBook>{
+abstract class LibraryBook extends Book implements Comparable<LibraryBook>{
     private String callNumber;
     public LibraryBook(String a,String t, String i, String c){
-	super(String a,String t, String i);
-	callNumber = c;
+    super(a,t,i);
+    callNumber = c;
     }
     public String getCall(){
-	return callNumber;
+    return callNumber;
     }
     public void setCall(String s){
-	callNumber = s;
+    callNumber = s;
     }
-    abstract void checkout(String patron, String due){
+    abstract void checkout(String patron, String due);
+    abstract void returned();
+    abstract String circulationStatus();
+    public int compareTo(LibraryBook l){
+    String n = callNumber;
+    String n2 = l.getCall();
+    if (n.equals(n2))return 0;
+    else if (n > n2)return 1;
+    else return -1;
     }
-    abstract void returned(){
+    public String toString(){
+    return super.toString()+ ", " +circulationStatus() + ", " + callNumber;
     }
-    abstract String circulationStatus(){
+}
+public class ReferenceBook extends LibraryBook{
+    private String  collection;
+    public ReferenceBook(String a,String t, String i, String c, String co){
+    super(a,t,i,c);
+    collection = co;
     }
-    public int compareTo(){
+    public String getCollection(){
+    return collection;
     }
-    public string toString(){
-	return super.toString +circulation
+    public void setCollection(String c){
+    collection = c;
+    }
+    public void checkout(){
+    System.out.println( "cannot check out a reference book");
+    }
+    public void returned(){
+    System.out.println("reference book could not have been checked out -- return impossible");
+    }
+    public String circulationStatus(){
+    return"non-circulating reference book";
+    }
+    public String toString(){
+    return super.toString()+", "+ collection;
     }
 }
