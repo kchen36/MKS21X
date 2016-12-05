5e1111ebc88db1f8150c332a4b27929cf4a85387
kchen36 <kchen36@stuy.edu>
2016-12-04 20:48:39 -0500

    version 2
    after iterator()

--- 02SuperArrayIterator/SuperArrayIterator.java
+++ 02SuperArrayIterator/SuperArrayIterator.java
@@ -1,3 +1,26 @@
+import java.util.*;
 public class SuperArrayIterator implements Iterator<String>{
-    
+    private int index = 0;
+    private SuperArray data;
+    public SuperArrayIterator(){
+		data = new SuperArray();
+		index = 0;
+    }
+    public SuperArrayIterator(SuperArray ary){
+	data = ary;
+    }
+    public void remove(){
+	throw new UnsupportedOperationException();
+    }
+    public boolean hasNext(){
+        return index < data.size();
+    }
+    public String next(){
+	if (hasNext()){
+	    index ++;
+	    return data.getVal(index - 1);
+	}else{
+	    throw new NoSuchElementException();
+	}
+    }
 }
