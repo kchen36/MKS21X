ffe4c4f3740f551672ae716d2ef2c4f7abae1e30
Kerry Chen <kchen36@stuy.edu>
2016-12-13 05:53:41 -0500

    version 1

---
+++ 04Sorts/Sorts.java
@@ -0,0 +1,25 @@
+public class Sorts{
+    public static String names(){
+	return "09.Chen.Kerry";
+    }
+    public static void selectionSort(int[] data){
+	for(int x = 0; x < data.length - 1;x++){
+	    int val = data[x];
+	    int y;
+	    for(y = x+1;y < data.length; y ++){
+		if (data[y] > val){
+		    val = data[y];
+		}
+	    } 
+	    data[y] = data[x];
+	    data[x] = val;
+	}
+    }
+    public static void main(String[]args){
+	int[] vals = {1,5,6,3,12,13,14,0,1};
+	selectionSort(vals);
+	for(int x = 0; x < vals.length;x++){
+	    System.out.println(vals[x]);
+	}
+    }
+}
