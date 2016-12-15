293bef0c81975cb889c5f2b2e1dfdf50b2484ecb
Kerry Chen <kchen36@stuy.edu>
2016-12-15 05:48:40 -0500

    version with bubblesort

--- 04Sorts/Sorts.java
+++ 04Sorts/Sorts.java
@@ -28,9 +28,25 @@ public class Sorts{
 	    }
 	}
     }
+    public static void bubbleSort(int[] data){
+	int swap = 1;
+	while(swap != 0){ 
+	    swap = 0;
+	    int pass = 0;
+	    for(int x = 0; x < data.length - (1 + pass)  ;x++){
+		if (data[x] > data[x+1]){
+		    int temp = data[x];
+		    data[x] = data[x+1];
+		    data[x+1] = temp;
+		    swap++;
+		    pass++;
+		}
+	    }
+	}
+    }
     public static void main(String[]args){
 	int[] vals = {1,5,6,3,-12,12,111,14,0,1};
-	insertionSort(vals);
+	bubbleSort(vals);
 	for(int x = 0; x < vals.length;x++){
 	    System.out.println(vals[x]);
 	}
